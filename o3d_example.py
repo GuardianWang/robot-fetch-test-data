b5c1b94fa53ab492d9060442f7eed65f4c122c27
Jason Liu <xinyu_liu@brown.edu>
2022-04-28 23:04:05 -0400

    intrinsic

--- o3d_example.py
+++ o3d_example.py
@@ -1,23 +1,30 @@
 import open3d as o3d
 import numpy as np
+import math
 
 rgb = o3d.io.read_image("image.jpg")
 depth = o3d.io.read_image("depth.png")
 rgbd = o3d.geometry.RGBDImage()
 rgbd = rgbd.create_from_color_and_depth(rgb, depth, convert_rgb_to_intensity=False)
 
+height, width, *_ = np.asarray(rgb).shape
+fov_y = 45
+fx = fy = 0.5 * height / math.tan(math.radians(fov_y * 0.5))
+cx, cy = width / 2, height / 2
+intrinsic = o3d.camera.PinholeCameraIntrinsic(width, height, fx, fy, cx, cy)
+
 pcd = o3d.geometry.PointCloud()
 
 pcd = pcd.create_from_rgbd_image(
     image=rgbd,
-    intrinsic=o3d.camera.PinholeCameraIntrinsic(o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault),
+    intrinsic=intrinsic,
     extrinsic=np.eye(4)
 )
 
 # pcd = pcd.create_from_depth_image(
-#              depth=depth,
-#              intrinsic=o3d.camera.PinholeCameraIntrinsic(o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault),
-#              extrinsic=np.eye(4)
+#     depth=depth,
+#     intrinsic=intrinsic,
+#     extrinsic=np.eye(4)
 # )
 
 # Each point corresponds to depth image in row-order
@@ -25,5 +32,5 @@ pcd = pcd.create_from_rgbd_image(
 # np_pcd = np.asarray(pcd.points)[:100_000]
 # pcd.points = o3d.utility.Vector3dVector(np_pcd)
 
-# o3d.io.write_point_cloud("point-cloud.pcd", pcd, write_ascii=True, print_progress=True)
+o3d.io.write_point_cloud("point-cloud.pcd", pcd, write_ascii=True, print_progress=True)
 o3d.visualization.draw_geometries([pcd])
